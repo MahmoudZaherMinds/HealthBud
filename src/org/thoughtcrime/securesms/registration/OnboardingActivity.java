dca5cf6fd967703c84e24503301165f580ab57f0
a.matter <amatter.eg@gmail.com>
2019-07-04 14:19:16 +0200

    initial commit

---
+++ src/org/thoughtcrime/securesms/registration/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package org.thoughtcrime.securesms.registration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.thoughtcrime.securesms.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
